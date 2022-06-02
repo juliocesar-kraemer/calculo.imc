51f7e6af6662fc9eec155617686219cc5c65420c
Júlio César Kraemer <jcesarkraemer@gmail.com>
2022-06-02 11:09:28 -0300

    commit 2 calc.imc

--- imc.js
+++ imc.js
@@ -2,3 +2,17 @@ let peso = 80;
 let altura = 1.8;
 
 let imc = peso / (altura * altura);
+
+if(imc <= 18.5){
+    alert("abaixo do peso");
+}else if(imc <= 24.9){
+    alert("peso normal");
+}else if(imc <= 29.9){
+    alert("sobrepeso");
+}else if(imc <= 34.9){
+    alert("obesidade grau I");
+}else if(imc <= 39.9){
+    alert("obesidade grau II");
+}else{
+    alert("obesidade grau III");
+}
